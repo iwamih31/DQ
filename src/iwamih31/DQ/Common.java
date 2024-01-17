f08117fd57648dae1d733f2c5173067e74c8a18d
iwamih31 <iwamih31@gmail.com>
2024-01-17 22:07:15 +0900

    /DQ/src/iwamih31/DQ/Common.java作成

---
+++ src/iwamih31/DQ/Common.java
@@ -0,0 +1,9 @@
+package iwamih31.DQ;
+
+public class Common {
+	public static void ___logOut___(String string) {
+		System.out.println("");
+		System.out.println(string);
+		System.out.println("");
+	}
+}
