26d3b808aba9766126a070073232e1a05c2c8949
iwamih31 <iwamih31@gmail.com>
2024-04-16 23:06:48 +0900

    Story.beBack()作成

--- src/iwamih31/DQ/Story.java
+++ src/iwamih31/DQ/Story.java
@@ -17,12 +17,16 @@ public class Story extends AbstractTableModel{
 
 	private static String[] textList;
 
+	static String p_Name = Main.getyName();
+	static String m_Name = Main.getmName();
+	static String b_Name = Main.getbName();
+
 	Story(){
 		textList = new String[16];
 		textList[0] = "";
-		textList[1] = "「 勇者[ " + Main.getyName() + " ]よ、よくぞ来てくれた・・・ 」";
-		textList[2] = "「 [ " + Main.getbName() + " ]と呼ばれる魔物が現れて、世界を滅ぼそうとしている・・・ 」";
-		textList[3] = "「 勇者[ " + Main.getyName() + " ]よ、[ " + Main.getbName() + " ]を倒し、世界に平和をもたらすのじゃ!!! 」";
+		textList[1] = "「勇者[ " + Main.getyName() + " ]よ、よくぞ来てくれた・・・ 」";
+		textList[2] = "「[ " + Main.getbName() + " ]と呼ばれる魔物が現れて、世界を滅ぼそうとしている・・・ 」";
+		textList[3] = "「勇者[ " + Main.getyName() + " ]よ、[ " + Main.getbName() + " ]を倒し、世界に平和をもたらすのじゃ!!! 」";
 		textList[4] = "";
 		textList[5] = "["+Main.getyName()+" は [" + Main.getG() +"G ] と道具を手に入れた";
 		textList[6] = "";
@@ -57,37 +61,30 @@ public class Story extends AbstractTableModel{
 	}
 
 	public static void beBack() {
-//		Battle.pTable();
-//		System.out.println( "" );
-//		System.out.println( "  おお!! [" + Main.getyName( ) + " ]よ、調子はどうだ？" );
-//		Input.ent();
-//		Battle.pTable();
-//		System.out.println( "" );
-//		System.out.println( "  [" + Main.getName( ) + "]の活躍は、我が耳にも色々と届いておるぞ・・・" );
-//		Input.ent();
-//		Battle.pTable();
-//		System.out.println( "" );
-//		System.out.print( "「 勇者[ " + Main.getyName() + " ]よ、一刻も早く[ " + Main.getbName() );
-//		System.out.println( " ]を倒し、世界に平和をもたらすのじゃ!!! 」");
-//		Input.ent();
+		Battle.pTable();
+		textList = new String[11];
+		textList[0] = "";
+		textList[1] = "「おお!! [" + Main.getyName( ) + " ]よ、調子はどうだ？」" ;
+		textList[2] = "";
+		textList[3] = "「[" + Main.getName( ) + "]の活躍は、我が耳にも色々と届いておるぞ・・・」";
+		textList[4] = "";
+		textList[5] = "「[ " + p_Name + " ]よ、一刻も早く[ " + b_Name + " ]を倒し、世界に平和をもたらすのじゃ!!!」";
+		textList[6] = "";
 	}
 
 	public static void relief() {
 		Battle.pTable();
-		String p_Name = Main.getyName();
-		String m_Name = Main.getmName();
-		String b_Name = Main.getbName();
 		textList = new String[11];
 		textList[0] = "";
-		textList[1] = "「 目覚めたか、[ " + p_Name + " ]よ・・・ 」";
+		textList[1] = "「目覚めたか、[ " + p_Name + " ]よ・・・」";
 		textList[2] = "";
-		textList[3] = "「 [ " + p_Name + " ]を全滅させるとは、[ " + m_Name + " ]は、やはり相当手強いのう・・・ 」";
+		textList[3] = "「[ " + p_Name + " ]を全滅させるとは、[ " + m_Name + " ]は、やはり相当手強いのう・・・」";
 		textList[4] = "";
-		textList[5] = ( " 「なんとか [ " + p_Name+ " ]達の命を救う事は出来たが、お金を大分消費してしまった・・・ 」");
+		textList[5] = "「なんとか [ " + p_Name+ " ]達の命を救う事は出来たが、お金を大分消費してしまった・・・」";
 		textList[6] = "";
-		textList[7] = " 「 だが[ " + p_Name + " ]の命を救えて、本当に何よりじゃ・・・ 」";
+		textList[7] = "「だが[ " + p_Name + " ]の命を救えて、本当に何よりじゃ・・・」";
 		textList[8] = "";
-		textList[9] = " 「 さあ、[ " + p_Name + " ]よ、一刻も早く[ " + b_Name + " ]を倒し、世界に平和をもたらすのじゃ!!! 」";
+		textList[9] = "「さあ、[ " + p_Name + " ]よ、一刻も早く[ " + b_Name + " ]を倒し、世界に平和をもたらすのじゃ!!!」";
 		textList[10] = "";
 	}
 
